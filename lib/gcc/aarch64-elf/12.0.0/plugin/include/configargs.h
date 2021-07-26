d1782430232ae1cf021932489818c4f4659c0ef6
AnGgIt86 <Jarbull86@gmail.com>
2021-07-26 09:19:04 +0530

    Import ARM64 GCC 20210725
    Build completed on: Mon Jul 26 09:19:04 IST 2021
    
    Configuration: Using built-in specs.
    COLLECT_GCC=./bin/aarch64-elf-gcc
    COLLECT_LTO_WRAPPER=/home/runner/work/Gcc-compiler/gcc-arm64/bin/../libexec/gcc/aarch64-elf/12.0.0/lto-wrapper
    Target: aarch64-elf
    Configured with: ../gcc/configure --target=aarch64-elf --prefix=/home/runner/work/Gcc-compiler/Gcc-compiler/../gcc-arm64 --disable-decimal-float --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --disable-docs --enable-default-ssp --enable-languages=c,c++ --with-pkgversion='NeedForSpeed GCC' --with-newlib --with-gnu-as --with-gnu-ld --with-sysroot
    Thread model: single
    Supported LTO compression algorithms: zlib
    gcc version 12.0.0 20210725 (experimental) (NeedForSpeed GCC)
    
    LLD: LLD 13.0.0 (https://github.com/llvm/llvm-project 5eb2e9aa64b7be7cd8ed7f36de19c2c9bdf1977c) (compatible with GNU linkers)
    Signed-off-by: AnGgIt86 <Jarbull86@gmail.com>

--- lib/gcc/aarch64-elf/12.0.0/plugin/include/configargs.h
+++ lib/gcc/aarch64-elf/12.0.0/plugin/include/configargs.h
@@ -1,5 +1,5 @@
 /* Generated automatically. */
-static const char configuration_arguments[] = "../gcc/configure --target=aarch64-elf --prefix=/home/runner/work/Gcc-compiler/Gcc-compiler/../gcc-arm64 --disable-decimal-float --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --disable-docs --enable-default-ssp --enable-languages=c,c++ --with-pkgversion='Eva GCC' --with-newlib --with-gnu-as --with-gnu-ld --with-sysroot";
+static const char configuration_arguments[] = "../gcc/configure --target=aarch64-elf --prefix=/home/runner/work/Gcc-compiler/Gcc-compiler/../gcc-arm64 --disable-decimal-float --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --disable-docs --enable-default-ssp --enable-languages=c,c++ --with-pkgversion='NeedForSpeed GCC' --with-newlib --with-gnu-as --with-gnu-ld --with-sysroot";
 static const char thread_model[] = "single";
 
 static const struct {
