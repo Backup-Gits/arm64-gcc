2db7bfc29ffd1bcefa6662125705c0c160b0668b
AnGgIt86 <jarbull86@gmail.com>
2021-08-19 23:09:32 +0700

    NeedForSpeed:GCC ARM64 bump to 20210819
    Build completed on: Thu Aug 19 23:09:32 WIB 2021
    
    Configuration: Using built-in specs.
    COLLECT_GCC=./bin/aarch64-elf-gcc
    COLLECT_LTO_WRAPPER=/home/runner/work/Gcc-compiler/gcc-arm64/bin/../libexec/gcc/aarch64-elf/12.0.0/lto-wrapper
    Target: aarch64-elf
    Configured with: ../gcc/configure --target=aarch64-elf --prefix=/home/runner/work/Gcc-compiler/Gcc-compiler/../gcc-arm64 --disable-decimal-float --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --disable-docs --enable-default-ssp --enable-languages=c,c++ --with-pkgversion='NeedForSpeed GCC' --with-newlib --with-gnu-as --with-gnu-ld --with-sysroot
    Thread model: single
    Supported LTO compression algorithms: zlib
    gcc version 12.0.0 20210819 (experimental) (NeedForSpeed GCC)
    
    LLD: LLD 14.0.0 (compatible with GNU linkers)
    Signed-off-by: AnGgIt86 <jarbull86@gmail.com>

--- lib/gcc/aarch64-elf/12.0.0/plugin/include/version.h
+++ lib/gcc/aarch64-elf/12.0.0/plugin/include/version.h
@@ -9,7 +9,7 @@
 BASEVER, DATESTAMP, DEVPHASE, and REVISION are defined by the
 Makefile.  */
 
-#define version_string "12.0.0 20210817 (experimental)"
+#define version_string "12.0.0 20210819 (experimental)"
 #define pkgversion_string "(NeedForSpeed GCC) "
 
 /* This is the location of the online document giving instructions for
